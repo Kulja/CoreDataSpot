e3eb597c1ec04c09601b27e239beb8602afdfbb5
Marko Kuljanski <Kulja@Markos-Mac.local>
2013-03-08 11:09:38 +0100

    Initial Commit

---
+++ Core Data SPoT/SPoTViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPoTViewController.h
+//  Core Data SPoT
+//
+//  Created by Marko Kuljanski on 3/8/13.
+//  Copyright (c) 2013 Marko Kuljanski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPoTViewController : UIViewController
+
+@end
---
+++ Core Data SPoT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Core Data SPoT
+//
+//  Created by Marko Kuljanski on 3/8/13.
+//  Copyright (c) 2013 Marko Kuljanski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPoTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPoTAppDelegate class]));
+    }
+}
